f4e0bd32bd75af31d0365e776a0c6869e637a4c6
unknown <myrnamares90@gmail.com>
2019-09-09 21:58:33 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function display(){
+    document.querySelector('.conteiner-viwes').style.display = 'block';
+    document.querySelector('.conteiner-menu').style.display = 'none';
+}
+
+
---
+++ navigation.js
@@ -0,0 +1,22 @@
+/*let menu = document.querySelector('#conteiner-menu');
+let home = document.querySelector('#conteiner-home');
+let service = document.querySelector('#conteiner-service');
+let we = document.querySelector('#conteiner-we');
+let portafolio = document.querySelector('#conteiner-portafolio');
+
+
+let routes = {
+    '': menu,
+    '#home': home,
+    '#service': service,
+    '#we': we,
+    'portafolio': portafolio
+}
+
+let contentAll = document.querySelector('#conteiner-all');
+
+window.addEventListener("haschange", ()=>{
+    contentAll.innerHTML = routes[window.location.hash];
+})
+
+contentAll.innerHTML = routes[window.location.hash]*/
